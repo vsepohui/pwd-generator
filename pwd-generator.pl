e960cd6e65d5113d25fe26ad7c25a8695e669191
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-05-16 23:05:44 +0500

    Final

--- pwd-generator.pl
+++ pwd-generator.pl
@@ -1,3 +1,16 @@
 #!/usr/bin/perl
 
 use 5.022;
+use warnings;
+
+use Digest::CRC qw(crc32);
+
+print "Enter old password: ";
+my $old = <>;
+chomp $old;
+
+$old .= time();
+
+srand crc32($old);
+
+say map { ('a'..'z', 'A'..'Z', 0..9)[rand 62] } 1..12;
